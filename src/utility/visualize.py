d8c3a322fad8bf8f7183ee6c6acdfcdaada97b09
Daniel Carreón <danielcarreong00@gmail.com>
2023-10-10 13:05:33 -0600

    ew

--- src/utility/visualize.py
+++ src/utility/visualize.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def plot_predicted_vs_true(y_test, y_pred, sort=True):
     """
     Plot the results from a regression model in a plot to compare the prediction vs. acutal values
-    
+
     Args:
         y_test : actual values
         y_pred : model predictions
@@ -14,7 +14,7 @@ def plot_predicted_vs_true(y_test, y_pred, sort=True):
     """
     # Create canvas
     plt.figure(figsize=(20, 5))
-    
+
     t = pd.DataFrame({"y_pred": y_pred, "y_test": y_test})
     if sort:
         t = t.sort_values(by=["y_test"])
@@ -38,7 +38,7 @@ def regression_scatter(y_test, y_pred):
     """
     Plot the results from a regression model in a scatter plot to compare the prediction vs. acutal values.
     Additionally, plots the regression line and ideal fit line.
-    
+
     Args:
         y_test : actual values
         y_pred : model predictions
@@ -71,19 +71,18 @@ def regression_scatter(y_test, y_pred):
 
 def plot_residuals(y_test, y_pred, bins=25):
     """
-    Plot residuals of a regression model. A good model will have a residuals 
+    Plot residuals of a regression model. A good model will have a residuals
     distribution that peaks at zero with few residuals at the extremes.
-    
+
     Args:
         y_test : actual values
         y_pred : model predictions
         bins (int, optional). Defaults to 25.
     """
-    
+
     residuals = y_test - y_pred
-    
+
     plt.figure(figsize=(20, 5))
     plt.hist(residuals, bins=bins, rwidth=0.95)
     plt.title("Residual Histogram")
     plt.show()
-     
